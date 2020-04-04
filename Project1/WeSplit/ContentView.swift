6badc99893e795cf4c82cae5cd877b1dfb6b8ab1
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-04 19:33:48 +0530

    Start project 1

---
+++ Project1/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Prathamesh Kowarkar on 04/04/20.
+//  Copyright © 2020 Prathamesh Kowarkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
