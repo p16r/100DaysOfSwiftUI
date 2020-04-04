b75f7e7183d519b16b02145193b1bd004e1251ba
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-04 19:40:05 +0530

    Add a navigation bar

--- Project1/WeSplit/ContentView.swift
+++ Project1/WeSplit/ContentView.swift
@@ -10,15 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, World!")
+        NavigationView{
+            Form {
+                Section {
+                    Text("Hello, World!")
+                }
+                Section {
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
             }
-
-            Section {
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
+            .navigationBarTitle("SwiftUI")
         }
     }
 }
