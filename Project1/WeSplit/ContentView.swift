a07a72039cb8e15dde6e50684b5261946f7fc68f
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-05 12:48:22 +0530

    Create a picker in a form

--- Project1/WeSplit/ContentView.swift
+++ Project1/WeSplit/ContentView.swift
@@ -17,14 +17,22 @@ struct ContentView: View {
     private let tipPercentages = [10, 15, 20, 25, 0]
 
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
-            Section {
-                Text("$\(checkAmount)")
-            }
+            .navigationBarTitle("WeSplit")
         }
     }
 
