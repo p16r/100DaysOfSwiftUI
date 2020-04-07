7f0129a272cde89a7460a01aab96358ecd08f205
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-07 21:33:24 +0530

    Complete challenge 1

--- Challenge1/Converter/ContentView.swift
+++ Challenge1/Converter/ContentView.swift
@@ -9,9 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+
+    @State private var inputValue = "1"
+    @State private var inputUnit = 1
+    @State private var outputUnit = 1
+
+    // Conversion mapping of each unit to metres
+    private let units: KeyValuePairs<String, Double> = [
+        "mm": 0.001,
+        "m": 1,
+        "km": 1000,
+        "in": 0.0254,
+        "ft": 0.3048,
+        "mi": 1609.344,
+    ]
+
+    private var outputValue: Double {
+        guard let input = Double(inputValue) else { return 0 }
+        return input * units[inputUnit].value / units[outputUnit].value
     }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Input")) {
+                    TextField("Value", text: $inputValue)
+                    Picker("Unit", selection: $inputUnit) {
+                        ForEach(0..<units.count) {
+                            Text(self.units[$0].key)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Output")) {
+                    Picker("Unit", selection: $outputUnit) {
+                        ForEach(0..<units.count) {
+                            Text(self.units[$0].key)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    Text(String(describing: outputValue))
+                }
+            }
+            .navigationBarTitle("Converter")
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
