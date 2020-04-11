6f7d107413e04c5f4d2cb89a5302bd890be5b5e0
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-11 16:54:05 +0530

    Start Project 3

---
+++ Project3/Project3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project3
+//
+//  Created by Prathamesh Kowarkar on 11/04/20.
+//  Copyright © 2020 Prathamesh Kowarkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
