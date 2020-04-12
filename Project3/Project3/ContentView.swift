a5cc875c8794d4ab2a4d86cc4de5f89b52da4fc5
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-12 18:03:24 +0530

    Create custom ViewModifier that adds a title to any view.

--- Project3/Project3/ContentView.swift
+++ Project3/Project3/ContentView.swift
@@ -8,28 +8,26 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
+struct Title: ViewModifier {
 
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
+    let text: String
 
-    var body: some View {
+    func body(content: Content) -> some View {
         VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<self.columns, id: \.self) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
+            Text(text)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+            content
         }
     }
 
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
+}
+
+extension View {
+
+    func title(_ title: String) -> some View {
+        modifier(Title(text: title))
     }
 
 }
@@ -37,11 +35,8 @@ struct GridStack<Content: View>: View {
 struct ContentView: View {
 
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            Text("R\(row)C\(col)")
-                .font(.system(.body, design: .monospaced))
-        }
+        Text("Hello, world!")
+            .title("Title")
     }
 
 }
