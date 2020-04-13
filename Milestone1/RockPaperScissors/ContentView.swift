e1cc36bb8ab8daf78744910412f4bc9abcdbd04b
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-13 21:01:56 +0530

    Start milestone 1

---
+++ Milestone1/RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Prathamesh Kowarkar on 13/04/20.
+//  Copyright © 2020 Prathamesh Kowarkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
