80a1f72dce89bf0097172d1f29c5b8d369eb2521
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-04-14 19:56:47 +0530

    Start Project 4

---
+++ Project4/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Prathamesh Kowarkar on 14/04/20.
+//  Copyright © 2020 Prathamesh Kowarkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
