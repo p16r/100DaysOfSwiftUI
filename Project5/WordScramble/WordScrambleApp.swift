596b91ad2fe430573eafbe6e58d0310b36665a33
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-06-27 20:21:54 +0530

    Start Project 5

---
+++ Project5/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Prathamesh Kowarkar on 27/06/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
