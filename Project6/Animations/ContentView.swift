136f52df20f586840b06128882827ee457975fa1
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-07-05 15:31:06 +0530

    Animate bindings

--- Project6/Animations/ContentView.swift
+++ Project6/Animations/ContentView.swift
@@ -12,26 +12,24 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1
 
     var body: some View {
-        Button("Tap") {
-//            animationAmount += 1
-        }
-        .padding(32)
-        .background(Color.accentColor)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.accentColor)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-        )
-        .animation(
-            Animation
-                .easeInOut(duration: 1)
-                .repeatForever(autoreverses: false)
-        )
-        .onAppear {
-            animationAmount = 2
+        VStack {
+            Stepper(
+                "Scale amount: \(animationAmount)",
+                value: $animationAmount.animation(
+                    Animation.easeInOut(duration: 1)
+                        .repeatCount(3, autoreverses: true)
+                ),
+                in: 1...10
+            )
+            Spacer()
+            Button("Tap") {
+                animationAmount += 1
+            }
+            .padding(32)
+            .background(Color.accentColor)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount, anchor: .bottom)
         }
     }
 
