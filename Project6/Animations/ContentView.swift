89bdcbbdd8b6608bedcedfb2226525f1d82e5c9e
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-07-05 12:28:28 +0530

    Create implicit animations

--- Project6/Animations/ContentView.swift
+++ Project6/Animations/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var animationAmount: CGFloat = 1
+
     var body: some View {
-        Text("Hello, world!").padding()
+        Button("Tap") {
+            animationAmount += 1
+        }
+        .padding(32)
+        .background(Color.accentColor)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 2)
+        .animation(.default)
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
