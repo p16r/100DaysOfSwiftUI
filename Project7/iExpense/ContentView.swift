56851bf8ce97915595494f363dea3c7f7dae51db
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-07-15 19:26:23 +0530

    Show and hide views

--- Project7/iExpense/ContentView.swift
+++ Project7/iExpense/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+
+    @Environment(\.presentationMode) var presentationMode
+
+    let name: String
+
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            Text("Hello, \(name)!")
+            Button("Dismiss") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
     }
+
+}
+
+struct ContentView: View {
+
+    @State var isShowingSheet = false
+
+    var body: some View {
+        Button("Show Sheet") {
+            isShowingSheet = true
+        }
+        .sheet(isPresented: $isShowingSheet) {
+            SecondView(name: "@prtmshk")
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
