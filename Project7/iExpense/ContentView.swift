fd572fd6d1b6cc0b8dbfc20f889cd22f52ff6acb
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-07-15 20:17:06 +0530

    Archive Swift objects with Codable

--- Project7/iExpense/ContentView.swift
+++ Project7/iExpense/ContentView.swift
@@ -7,14 +7,25 @@
 
 import SwiftUI
 
+struct User {
+
+    let firstName: String
+    let lastName: String
+
+}
+
 struct ContentView: View {
 
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "taps")
+    @State private var user = User(
+        firstName: "Prathamesh",
+        lastName: "Kowarkar"
+    )
 
     var body: some View {
-        Button("Taps: \(tapCount)") {
-            tapCount += 1
-            UserDefaults.standard.set(tapCount, forKey: "taps")
+        Button("Save User") {
+            if let data = try? JSONEncoder().encode(user) {
+                UserDefaults.standard.set(data, forKey: "user")
+            }
         }
     }
 
