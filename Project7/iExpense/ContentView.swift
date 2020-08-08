79ddfdf40ae15760d6411ee64ab9ede50e66a863
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-08-08 14:05:19 +0530

    Add coloured formatting to expense amount

--- Project7/iExpense/ContentView.swift
+++ Project7/iExpense/ContentView.swift
@@ -94,6 +94,18 @@ struct ExpenseView: View {
             }
             Spacer()
             Text("$\(expense.amount)")
+                .foregroundColor(color)
+                .padding(.horizontal, 4)
+                .background(color.opacity(0.2))
+                .clipShape(RoundedRectangle(cornerRadius: 4))
+        }
+    }
+
+    var color: Color {
+        switch expense.amount {
+            case 0..<10: return .green
+            case 10..<100: return .orange
+            default: return .red
         }
     }
 
