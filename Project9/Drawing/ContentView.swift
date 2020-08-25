05d315084a24d970f90f4a040131b3f7680b4c21
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-08-25 16:16:20 +0530

    Start Project 9

---
+++ Project9/Drawing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Prathamesh Kowarkar on 25/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project9/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Prathamesh Kowarkar on 25/08/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
