e2371060fa5663ee9feee8f0e4bdfa47e621454f
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-08-26 21:42:51 +0530

    Enable high-performance Metal rendering with drawingGroup()

--- Project9/Drawing/ContentView.swift
+++ Project9/Drawing/ContentView.swift
@@ -9,34 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
 
     var body: some View {
         VStack {
-            Text("Hello World")
+            ColorCyclingView(amount: colorCycle)
                 .frame(width: 300, height: 300)
-                .border(
-                    ImagePaint(
-                        image: Image("Example"),
-                        sourceRect: CGRect(
-                            x: 0,
-                            y: 0.25,
-                            width: 1,
-                            height: 0.5
-                        ),
-                        scale: 0.1
-                    ),
-                    width: 30
-                )
-            Capsule()
-                .strokeBorder(
-                    ImagePaint(image: Image("Example"), scale: 0.1),
-                    lineWidth: 20
-                )
-                .frame(width: 300, height: 200)
+            Slider(value: $colorCycle)
         }
     }
+}
+
+struct ColorCyclingView: View {
+
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [
+                                    self.color(for: value, brightness: 1),
+                                    self.color(for: value, brightness: 0.5)
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
