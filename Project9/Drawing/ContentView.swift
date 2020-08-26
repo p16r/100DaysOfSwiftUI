86940fa59d00cb077e899565606873c21bfca223
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-08-26 21:13:01 +0530

    Creative borders and fills using ImagePaint

--- Project9/Drawing/ContentView.swift
+++ Project9/Drawing/ContentView.swift
@@ -14,43 +14,29 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            Text("Width")
-            Slider(value: $petalWidth, in: -100...100)
-                .padding([.horizontal, .bottom])
+            Text("Hello World")
+                .frame(width: 300, height: 300)
+                .border(
+                    ImagePaint(
+                        image: Image("Example"),
+                        sourceRect: CGRect(
+                            x: 0,
+                            y: 0.25,
+                            width: 1,
+                            height: 0.5
+                        ),
+                        scale: 0.1
+                    ),
+                    width: 30
+                )
+            Capsule()
+                .strokeBorder(
+                    ImagePaint(image: Image("Example"), scale: 0.1),
+                    lineWidth: 20
+                )
+                .frame(width: 300, height: 200)
         }
     }
-}
-
-struct Flower: Shape {
-
-    var petalOffset: Double = -20
-    var petalWidth: Double = 100
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        for number in stride(from: 0, to: 2 * CGFloat.pi, by: .pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(
-                .init(translationX: rect.width / 2, y: rect.height / 2)
-            )
-            let rect = CGRect(
-                x: CGFloat(petalOffset),
-                y: 0,
-                width: CGFloat(petalWidth),
-                height: rect.width / 2
-            )
-            let originalPetal = Path(ellipseIn: rect)
-            let rotatedPetal = originalPetal.applying(position)
-            path.addPath(rotatedPetal)
-        }
-        return path
-    }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
