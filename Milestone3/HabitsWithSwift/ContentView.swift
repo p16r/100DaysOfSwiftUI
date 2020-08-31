4310c5d6b6508b46ea5accd4234756d42f82f291
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-08-31 20:05:24 +0530

    Start milestone 3

---
+++ Milestone3/HabitsWithSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitsWithSwift
+//
+//  Created by Prathamesh Kowarkar on 31/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Milestone3/HabitsWithSwift/HabitsWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsWithSwiftApp.swift
+//  HabitsWithSwift
+//
+//  Created by Prathamesh Kowarkar on 31/08/20.
+//
+
+import SwiftUI
+
+@main
+struct HabitsWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
