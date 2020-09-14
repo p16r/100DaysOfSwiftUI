84a05f908413fb27d4d3b91eb98f7ec8dece40f6
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-09-14 11:41:12 +0530

    Add Codable conformance for @Published properties

--- Project10/Cupcake Corner/ContentView.swift
+++ Project10/Cupcake Corner/ContentView.swift
@@ -7,6 +7,28 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+
+    enum CodingKeys: CodingKey {
+
+        case name
+
+    }
+
+    @Published var name: String
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
