7ad4d581e960046b3485a6bd0efa27af832604d2
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-09-14 10:53:55 +0530

    Start project 10

---
+++ Project10/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Prathamesh Kowarkar on 14/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
