75833e8e619f3db8a00ae7f406c78f7df57149ae
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-09-18 18:31:29 +0530

    Create books with Core Data

---
+++ Project11/Bookworm/AddBookView.swift
@@ -0,0 +1,77 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Prathamesh Kowarkar on 18/09/20.
+//  Copyright © 2020 Prathamesh Kowarkar. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct AddBookView: View {
+
+    @Environment(\.managedObjectContext) var context
+    @Environment(\.presentationMode) var presentationMode
+
+    @State private var title = ""
+    @State private var author = ""
+    @State private var genre = ""
+    @State private var review = ""
+    @State private var rating: Int16 = 3
+
+    let genres = [
+        "Fantasy",
+        "Horror",
+        "Kids",
+        "Mystery",
+        "Poetry",
+        "Romance",
+        "Thriller",
+    ]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of the book", text: $title)
+                    TextField("Author of the book", text: $author)
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) { genre in
+                            Text(genre)
+                        }
+                    }
+                }
+                Section {
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text("\($0)")
+                        }
+                    }
+                    TextField("Write a review", text: $review)
+                }
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: context)
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = rating
+                        newBook.genre = genre
+                        newBook.review = review
+                        try? context.save()
+
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+            .navigationBarTitle("Add a book")
+        }
+    }
+
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
--- Project11/Bookworm/ContentView.swift
+++ Project11/Bookworm/ContentView.swift
@@ -11,26 +11,26 @@ import SwiftUI
 struct ContentView: View {
 
     @Environment(\.managedObjectContext) var context
-    @FetchRequest(entity: Student.entity(), sortDescriptors: [])
-    var students: FetchedResults<Student>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [])
+    var books: FetchedResults<Book>
+
+    @State private var showingAddScreen = false
 
     var body: some View {
-        VStack {
-            List {
-                ForEach(students, id: \.id) { student in
-                    Text(student.name ?? "Unknown")
+        NavigationView {
+            Text("Count: \(books.count)")
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(
+                    trailing: Button {
+                        showingAddScreen = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                )
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                        .environment(\.managedObjectContext, context)
                 }
-            }
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-                let student = Student(context: context)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                try? context.save()
-            }
         }
     }
 
