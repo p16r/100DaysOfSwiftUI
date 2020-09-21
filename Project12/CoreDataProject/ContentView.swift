7b1309498785bcc4a4c6ef813bb3c8dd9bf06fb5
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-09-21 23:08:39 +0530

    Make FilteredList accept an array of NSSortDescriptor

--- Project12/CoreDataProject/ContentView.swift
+++ Project12/CoreDataProject/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Prathamesh Kowarkar on 20/09/20.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
@@ -12,43 +13,73 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) var context
     @FetchRequest(entity: Country.entity(), sortDescriptors: [])
     var countries: FetchedResults<Country>
+    @State var filter: (key: String, value: String)? = nil
+    @State var ascending: Bool? = nil
+
+    var sortDescriptors: [NSSortDescriptor] {
+        guard let ascending = ascending else { return [] }
+        return [
+            NSSortDescriptor(keyPath: \Country.fullName, ascending: ascending)
+        ]
+    }
 
     var body: some View {
-        List {
-            ForEach(countries, id: \.self) { country in
-                Section(header: Text(country.wrappedFullName)) {
-                    ForEach(country.candyArray, id: \.self) { candy in
-                        Text(candy.wrappedName)
-                    }
+        FilteredList(
+            filter: filter,
+            sortKeys: sortDescriptors
+        ) { (country: Country) in
+            Text(country.fullName ?? "~")
+        }
+        HStack {
+            VStack {
+                Button("Show UK") {
+                    filter = ("shortName", "UK")
+                    ascending = nil
+                }
+                Button("Show Switzerland") {
+                    filter = ("shortName", "CH")
+                    ascending = nil
+                }
+            }
+            VStack {
+                Button("Sort A-Z") {
+                    filter = nil
+                    ascending = true
+                }
+                Button("Show Z-A") {
+                    filter = nil
+                    ascending = false
                 }
             }
         }
-        Button("Add") {
-            let candy1 = Candy(context: context)
-            candy1.name = "Mars"
-            candy1.origin = Country(context: context)
-            candy1.origin?.shortName = "UK"
-            candy1.origin?.fullName = "United Kingdom"
+        if countries.isEmpty {
+            Button("Add") {
+                let candy1 = Candy(context: context)
+                candy1.name = "Mars"
+                candy1.origin = Country(context: context)
+                candy1.origin?.shortName = "UK"
+                candy1.origin?.fullName = "United Kingdom"
 
-            let candy2 = Candy(context: context)
-            candy2.name = "KitKat"
-            candy2.origin = Country(context: context)
-            candy2.origin?.shortName = "UK"
-            candy2.origin?.fullName = "United Kingdom"
+                let candy2 = Candy(context: context)
+                candy2.name = "KitKat"
+                candy2.origin = Country(context: context)
+                candy2.origin?.shortName = "UK"
+                candy2.origin?.fullName = "United Kingdom"
 
-            let candy3 = Candy(context: context)
-            candy3.name = "Twix"
-            candy3.origin = Country(context: context)
-            candy3.origin?.shortName = "UK"
-            candy3.origin?.fullName = "United Kingdom"
+                let candy3 = Candy(context: context)
+                candy3.name = "Twix"
+                candy3.origin = Country(context: context)
+                candy3.origin?.shortName = "UK"
+                candy3.origin?.fullName = "United Kingdom"
 
-            let candy4 = Candy(context: context)
-            candy4.name = "Toblerone"
-            candy4.origin = Country(context: context)
-            candy4.origin?.shortName = "CH"
-            candy4.origin?.fullName = "Switzerland"
+                let candy4 = Candy(context: context)
+                candy4.name = "Toblerone"
+                candy4.origin = Country(context: context)
+                candy4.origin?.shortName = "CH"
+                candy4.origin?.fullName = "Switzerland"
 
-            try? context.save()
+                try? context.save()
+            }
         }
     }
 
---
+++ Project12/CoreDataProject/FilteredList.swift
@@ -0,0 +1,38 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Prathamesh Kowarkar on 20/09/20.
+//
+
+import CoreData
+import SwiftUI
+
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    
+    var fetchRequest: FetchRequest<T>
+    var content: (T) -> Content
+    
+    init(
+        filter: (key: String, value: String)? = nil,
+        sortKeys: [NSSortDescriptor] = [],
+        @ViewBuilder content: @escaping (T) -> Content
+    ) {
+        let predicate = filter.map {
+            NSPredicate(format: "%K BEGINSWITH[c] %@", $0.key, $0.value)
+        }
+        let sortDescriptors = sortKeys.map {
+            NSSortDescriptor(key: $0.key, ascending: $0.ascending)
+        }
+        fetchRequest = FetchRequest(
+            entity: T.entity(),
+            sortDescriptors: sortDescriptors,
+            predicate: predicate
+        )
+        self.content = content
+    }
+    
+    var body: some View {
+        List(fetchRequest.wrappedValue, id: \.self, rowContent: content)
+    }
+}
