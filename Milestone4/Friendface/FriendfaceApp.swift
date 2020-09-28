91a0c34e840abf10375269b066b2ad1ee254e460
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-09-28 15:42:52 +0530

    Start Milestone 4

---
+++ Milestone4/Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Prathamesh Kowarkar on 28/09/20.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
