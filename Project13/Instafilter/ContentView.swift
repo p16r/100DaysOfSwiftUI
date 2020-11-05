68d6fcf270fca227fabf32c57ad6edd6456d4f10
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-11-05 13:10:05 +0530

    Start Project 13

---
+++ Project13/Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Prathamesh Kowarkar on 05/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
