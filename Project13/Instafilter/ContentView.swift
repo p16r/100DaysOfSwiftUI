942634ee3a622089007678fb5542187230097802
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-11-05 17:29:06 +0530

    Integrate Core Image with SwiftUI

--- Project13/Instafilter/ContentView.swift
+++ Project13/Instafilter/ContentView.swift
@@ -5,32 +5,41 @@
 //  Created by Prathamesh Kowarkar on 05/11/20.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
+
+    @State private var image: Image?
 
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
-            }
-            .actionSheet(isPresented: $showingActionSheet) {
-                ActionSheet(
-                    title: Text("Change background"),
-                    message: Text("Select a new color"),
-                    buttons: [
-                        .default(Text("Red")) { backgroundColor = .red },
-                        .default(Text("Green")) { backgroundColor = .green },
-                        .default(Text("Blue")) { backgroundColor = .blue },
-                        .cancel()
-                    ]
-                )
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
     }
+
+    func loadImage() {
+
+        func filter(with image: UIImage) -> CIFilter {
+            let filter = CIFilter.twirlDistortion()
+            filter.inputImage = CIImage(image: image)
+            filter.radius = 400
+            filter.center = CGPoint(x: image.size.width / 2, y: image.size.height / 2)
+            return filter
+        }
+
+        image = UIImage(named: "Example")
+            .map { filter(with: $0) }
+            .flatMap { $0.outputImage }
+            .flatMap { CIContext().createCGImage($0, from: $0.extent) }
+            .map { UIImage(cgImage: $0) }
+            .map { Image(uiImage: $0) }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
