e62c062e46913f35ed0a2e939f11db268565ab22
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-11-12 21:09:13 +0530

    Build the basic UI

--- Project13/Instafilter/ContentView.swift
+++ Project13/Instafilter/ContentView.swift
@@ -12,27 +12,43 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
+    @State private var filterIntensity = 0.5
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            Button("Select Image") {
-                showingImagePicker = true
+        NavigationView {
+            VStack {
+                ZStack {
+                    Color.secondary
+                    if let image = image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture.")
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    //  select image
+                }
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                HStack {
+                    Button("Change filter") {
+                        //  change filter
+                    }
+                    Spacer()
+                    Button("Save") {
+                        //  save the picture
+                    }
+                }
             }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: $inputImage)
-        }
-    }
-
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        ImageSaver().writeToPhotoAlbum(image: inputImage)
     }
 
 }
